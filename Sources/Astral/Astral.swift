59c0ef2088c7729e717bee563fb6519134ce0f5e
Xiangyu Sun <xiangyu.sun@glovoapp.com>
2023-01-19 10:56:07 +0100

    Initial Commit

---
+++ Sources/Astral/Astral.swift
@@ -0,0 +1,6 @@
+public struct Astral {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/AstralTests/AstralTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Astral
+
+final class AstralTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Astral().text, "Hello, World!")
+    }
+}
