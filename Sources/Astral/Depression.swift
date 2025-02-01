3f3010234b6172110dd58cd9d8d810b4fc63c8c4
Xiangyu Sun <xiangyu.sun@glovoapp.com>
2025-02-01 23:52:10 +0100

    change depression value and got rid of 8 test failures

--- Sources/Astral/Depression.swift
+++ Sources/Astral/Depression.swift
@@ -2,45 +2,43 @@ import Foundation
 
 /// The depression angle in degrees for the dawn/dusk calculation
 enum Depression: ExpressibleByIntegerLiteral, RawRepresentable {
-
+  
   case civil
   case nautical
-  case astromomic
-
+  case astronomical  // Corrected spelling
   case other(Int)
-
+  
   // MARK: Lifecycle
-
+  
   init(rawValue value: Int) {
-    if value == 8 {
+    if value == 6 {
       self = .civil
     } else if value == 12 {
       self = .nautical
-    } else if value == 16 {
-      self = .astromomic
+    } else if value == 18 {
+      self = .astronomical
     } else {
       self = .other(value)
     }
   }
-
+  
   init(integerLiteral value: Int) {
     self.init(rawValue: value)
   }
-
+  
   // MARK: Internal
-
+  
   typealias RawValue = Int
-
   typealias IntegerLiteralType = Int
-
+  
   var rawValue: Int {
     switch self {
     case .civil:
-      return 8
+      return 6
     case .nautical:
       return 12
-    case .astromomic:
-      return 16
+    case .astronomical:
+      return 18
     case .other(let int):
       return int
     }
